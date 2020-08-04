159f07a3d225f5e668458181f4f0f5f76270151d
PriscilaCS <pry_chu@hotmail.com>
2020-08-03 23:55:58 -0300

    Aula 3

--- js/main.js
+++ js/main.js
@@ -39,9 +39,33 @@ console.log(lista.join("-"));
 var fruta = {nome: "maçã", cor: "vermelho"};
 console.log(fruta.nome);
 console.log(fruta.cor);
-
-*/
-
 var frutas = [{nome: "maçã", cor: "vermelho"}, {nome: "uva", cor: "verde"}];
 console.log(frutas[1].nome);
 console.log(frutas[0].cor);
+
+// *********** AULA 3 *************
+
+var idade = prompt("Qual a sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+
+var count = 0;
+while(count < 5){
+    alert(count);
+    console.log(count);
+    count ++;
+}
+
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+    console.log(count);
+}
+*/
+var d = new Date();
+console.log(d);
+console.log(d.getMonth() + 1);
+console.log(d.getDay());
