4f8ce40092384a75a56628fe19ee8b67d56c2ed8
PriscilaCS <pry_chu@hotmail.com>
2020-08-04 01:17:27 -0300

    Aula 4

--- js/main.js
+++ js/main.js
@@ -64,8 +64,35 @@ for(count = 0; count <= 5; count++){
     alert(count);
     console.log(count);
 }
-*/
+
 var d = new Date();
 console.log(d);
 console.log(d.getMonth() + 1);
 console.log(d.getDay());
+
+// *********** AULA 3 *************
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+console.log(soma(5,10));
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}*/
+
+var validar = 0;
+function validaIdade(idade) {    
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade");
+validaIdade(idade);
+console.log(validar);
+
+//console.log(setReplace("Vai Japão", "Japão", "Brasil"));
